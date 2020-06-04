c74f132019c0897f4fa391084ccce8420d479787
Jose Manuel Martinez <2015378@ce.pucmm.edu.do>
2020-06-03 23:31:29 -0400

    Update ClienteRepository.java

--- src/main/java/com/rental/app/backend/repositories/ClienteRepository.java
+++ src/main/java/com/rental/app/backend/repositories/ClienteRepository.java
@@ -1,6 +1,7 @@
 package com.rental.app.backend.repositories;
 
+import com.rental.app.backend.models.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository extends JpaRepository<ClienteRepository,Long> {
+public interface ClienteRepository extends JpaRepository<Cliente,Long> {
 }
